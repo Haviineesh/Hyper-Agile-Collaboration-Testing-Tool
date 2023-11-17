ab462c2cf81167a2996671259f4e2b50bfd05648
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-11-17 10:35:47 +0800

    Merge branch 'main' of https://github.com/Haviineesh/Hyper-Agile-Collaboration-Testing-Tool

--- demo/src/main/java/demo_ver/demo/controllers/LoginController.java
+++ demo/src/main/java/demo_ver/demo/controllers/LoginController.java
@@ -1,12 +1,20 @@
 package demo_ver.demo.controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
+    @GetMapping("/")
     public String login() {
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
         return "login";
     }
 }
