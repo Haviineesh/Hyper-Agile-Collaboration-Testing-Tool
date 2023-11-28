3abc6daccc1fdfe9d5e182d359bde68975e9812d
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-11-28 13:36:47 +0800

    create newTestCaseController

---
+++ demo/src/main/java/demo_ver/demo/controllers/NewTestCaseController.java
@@ -0,0 +1,13 @@
+package demo_ver.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class NewTestCaseController {
+    
+    @GetMapping("/add")
+    public String showAddTestCaseForm() {
+        return "addTestCase"; // Thymeleaf template name for the addTestCase.html file
+    }
+}
