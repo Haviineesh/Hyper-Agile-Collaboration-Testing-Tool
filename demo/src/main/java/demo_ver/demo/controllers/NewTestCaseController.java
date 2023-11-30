71979724ccb2f8f478e1e7e4c7d7c526c36e718e
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-01 01:21:12 +0800

    update CaseController

--- demo/src/main/java/demo_ver/demo/controllers/NewTestCaseController.java
+++ demo/src/main/java/demo_ver/demo/controllers/NewTestCaseController.java
@@ -1,13 +1,34 @@
 package demo_ver.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import demo_ver.demo.model.ViewCase;
+import demo_ver.demo.repo.ViewCaseData;
+
 
 @Controller
 public class NewTestCaseController {
+
+    @Autowired
+    ViewCaseData repo;
     
     @GetMapping("/add")
     public String showAddTestCaseForm() {
         return "addTestCase"; // Thymeleaf template name for the addTestCase.html file
     }  
+
+    @RequestMapping("/saveCase")
+    @ResponseBody
+    public String saveCase(ViewCase viewCase) {
+        repo.save(viewCase);
+        return "Success";
+    }
+    
+
 }
