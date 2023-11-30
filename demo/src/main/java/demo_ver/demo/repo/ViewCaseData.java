d6a1b9b9cd60061766c894eb88e6529421ba9dca
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-01 01:21:36 +0800

    create repo case

---
+++ demo/src/main/java/demo_ver/demo/repo/ViewCaseData.java
@@ -0,0 +1,10 @@
+package demo_ver.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import demo_ver.demo.model.ViewCase;
+
+public interface ViewCaseData extends JpaRepository<ViewCase,Integer>{
+    
+
+}
