e35a4f11818cb89b988bd3ddd61f1b733de154b0
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-01 18:28:07 +0800

    comment to revert changes

--- demo/src/main/java/demo_ver/demo/controllers/NewTestCaseController.java
+++ demo/src/main/java/demo_ver/demo/controllers/NewTestCaseController.java
@@ -8,27 +8,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import demo_ver.demo.model.ViewCase;
-import demo_ver.demo.repo.ViewCaseData;
+// import demo_ver.demo.model.ViewCase;
+// import demo_ver.demo.repo.ViewCaseData;
 
 
 @Controller
 public class NewTestCaseController {
 
-    @Autowired
-    ViewCaseData repo;
+    // @Autowired
+    // ViewCaseData repo;
     
     @GetMapping("/add")
     public String showAddTestCaseForm() {
         return "addTestCase"; // Thymeleaf template name for the addTestCase.html file
     }  
 
-    @RequestMapping("/saveCase")
-    @ResponseBody
-    public String saveCase(ViewCase viewCase) {
-        repo.save(viewCase);
-        return "Success";
-    }
+    // @RequestMapping("/saveCase")
+    // @ResponseBody
+    // public String saveCase(ViewCase viewCase) {
+    //     repo.save(viewCase);
+    //     return "Success";
+    // }
     
 
 }
--- demo/src/main/java/demo_ver/demo/repo/ViewCaseData.java
+++ demo/src/main/java/demo_ver/demo/repo/ViewCaseData.java
@@ -1,10 +1,10 @@
-package demo_ver.demo.repo;
+// package demo_ver.demo.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.JpaRepository;
 
-import demo_ver.demo.model.ViewCase;
+// import demo_ver.demo.model.ViewCase;
 
-public interface ViewCaseData extends JpaRepository<ViewCase,Integer>{
+// public interface ViewCaseData extends JpaRepository<ViewCase,Integer>{
     
 
-}
+// }
