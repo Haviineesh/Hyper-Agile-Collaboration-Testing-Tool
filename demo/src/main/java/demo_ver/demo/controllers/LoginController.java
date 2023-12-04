d7df47b3d75e80c561681666423212cbe4bf7ab9
teenesh10 <teeneshsubramaniam10@gmail.com>
2023-12-05 03:37:31 +0800

    small fix on login controller

--- demo/src/main/java/demo_ver/demo/controllers/LoginController.java
+++ demo/src/main/java/demo_ver/demo/controllers/LoginController.java
@@ -1,15 +1,19 @@
 package demo_ver.demo.controllers;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestBody;
+import demo_ver.demo.model.User;
 import demo_ver.demo.service.LoginService;
-import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class LoginController {
 
+   
     private final LoginService loginService;
 
     public LoginController(LoginService loginService) {
@@ -21,28 +25,31 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String login(
-            @RequestParam String username,
-            @RequestParam String password,
-            @RequestParam String roleId,
-            HttpServletRequest request
-    ) {
-        // Use the LoginService for login validation
-        if (loginService.validateLogin(username, password, roleId)) {
-            // Set a session attribute indicating successful login
-            request.getSession().setAttribute("loggedInUser", username);
-            // Redirect to the "viewTestCases" page upon successful login
-            return "redirect:/view";
-        } else {
-            // Redirect back to the login page with an error message
-            return "redirect:/login?error=true";
-        }
-    }
+    // @PostMapping("/login")
+    // public ResponseEntity<String> login(@RequestBody User loginUser) {
+    //     String username = loginUser.getUsername();
+    //     String password = loginUser.getPassword();
+    //     String roleId = loginUser.getRoleId();
 
-    @GetMapping("/changepassword")
-    public String showChangePasswordPage() {
-        return "ChangePassword";
+    //     if (loginService.validateLogin(username, password, roleId)) {
+
+    //         return ResponseEntity.ok("Login Successful!");
+    //     } else {
+    //         return ResponseEntity.status(401).body("Invalid username or password or roleId");
+    //     }
+    // }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@ModelAttribute User loginUser) {
+        String username = loginUser.getUsername();
+        String password = loginUser.getPassword();
+        String roleId = loginUser.getRoleId();
+
+        if (loginService.validateLogin(username, password, roleId)) {
+            return ResponseEntity.ok("Login Successful!");
+        } else {
+            return ResponseEntity.status(401).body("Invalid username or password or roleId");
+        }
     }
 
     @GetMapping("/view")
--- demo/src/main/java/demo_ver/demo/service/LoginService.java
+++ demo/src/main/java/demo_ver/demo/service/LoginService.java
@@ -6,10 +6,16 @@ import org.springframework.stereotype.Service;
 public class LoginService {
 
     public boolean validateLogin(String username, String password, String roleId) {
+        
+          // Log received values
+          System.out.println("Received username: " + username);
+          System.out.println("Received password: " + password);
+          System.out.println("Received roleId: " + roleId);
+          
         // Simulating login validation
         return "teen".equals(username) &&
-                "123admin".equals(password) &&
-                "admin".equals(roleId);
+                "123".equals(password) &&
+                "a10".equals(roleId);
     }
 }
 
