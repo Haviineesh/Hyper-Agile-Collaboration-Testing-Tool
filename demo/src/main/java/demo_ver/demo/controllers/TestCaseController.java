2692586717968494e908657d586b7871490631ba
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-05 15:02:32 +0800

    create testcase controller

---
+++ demo/src/main/java/demo_ver/demo/controllers/TestCaseController.java
@@ -0,0 +1,44 @@
+package demo_ver.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import demo_ver.demo.model.TestCase;
+import demo_ver.demo.service.ViewCaseService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+public class TestCaseController {
+
+    @Autowired
+    private ViewCaseService viewCaseService;
+
+
+    @GetMapping("/view")
+    public String viewCase(Model model) {
+        List<TestCase> cases = viewCaseService.getAllRoles();
+        model.addAttribute("cases", cases);
+        return "viewTestCase";
+    }
+
+    @GetMapping("/add")
+    public String showAddTestCaseForm(Model model) {
+        model.addAttribute("testCase", new TestCase());
+        return "addTestCase"; // Thymeleaf template name for the addTestCase.html file
+    }  
+
+    @PostMapping("/add")
+	public String addTestCaseForm(@ModelAttribute("testCase") TestCase testCase) {
+		viewCaseService.addTestCaseForm(testCase);// save product into database, using DbService
+		return "viewTestCase";
+	}
+    
+}
