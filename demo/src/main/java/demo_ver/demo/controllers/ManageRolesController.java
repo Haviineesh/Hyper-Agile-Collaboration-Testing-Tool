d934db9b5b68fbb9c6827295958c8d7f1ca7b6dd
Haviineesh <haviineesh@gmail.com>
2023-12-05 23:10:23 +0800

    Add roles. Problem list not found

--- demo/src/main/java/demo_ver/demo/controllers/ManageRolesController.java
+++ demo/src/main/java/demo_ver/demo/controllers/ManageRolesController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import demo_ver.demo.model.ManageRole;
 import demo_ver.demo.service.ManageRoleService;
@@ -18,13 +20,13 @@ public class ManageRolesController {
     // @GetMapping("/manageroles")
     // @ResponseBody
     // public List<ManageRole> getAllRoles(){
-    //     return manageRoleService.getAllRoles();
+    // return manageRoleService.getAllRoles();
     // }
 
     @GetMapping("/manageroles")
     public String manageroles(Model model) {
         List<ManageRole> roles = manageRoleService.getAllRoles();
-		model.addAttribute("roles", roles);
+        model.addAttribute("roles", roles);
         return "ManageRoles";
     }
 
@@ -34,9 +36,22 @@ public class ManageRolesController {
     }
 
     @GetMapping("/editrole")
-    public String managerolesedit(){
+    public String managerolesedit() {
         return "ManageRolesEdit";
     }
 
-    
+    @GetMapping("/createrole")
+    public String showCreateNewRole(Model model) {
+        model.addAttribute("manageRole", new ManageRole());
+        return "ManageRolesNew";
+    }
+
+    // @RequestMapping("/createrole")
+    @PostMapping("/createrole")
+    public String createrole(@ModelAttribute("manageRole") ManageRole manageRole, Model model) {
+        manageRoleService.addRole(manageRole);// save product into database, using DbService
+        		model.addAttribute("manageRole", manageRole);
+        return "ManageRoles";
+    }
+
 }
--- demo/src/main/java/demo_ver/demo/service/ManageRoleService.java
+++ demo/src/main/java/demo_ver/demo/service/ManageRoleService.java
@@ -1,11 +1,12 @@
 package demo_ver.demo.service;
 
-import org.springframework.stereotype.Service;
-import demo_ver.demo.model.ManageRole;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import demo_ver.demo.model.ManageRole;
 
 @Service
 public class ManageRoleService {
@@ -14,8 +15,20 @@ public class ManageRoleService {
             new ManageRole(4055, "Tester", "unit tester"),
             new ManageRole(5055, "Product Manager", "manage production")));
 
-    public static List<ManageRole> getAllRoles() {
+    public List<ManageRole> getAllRoles() {
         return new ArrayList<ManageRole>(roleList);
+    }
+
+    public void addRole(ManageRole newRole) {
+        // Check if a role with the same ID already exists
+        if (roleList.stream().noneMatch(role -> role.getRoleID() == newRole.getRoleID())) {
+            roleList.add(newRole);
+        } else {
+            // Handle the case when a role with the same ID already exists
+            // You can throw an exception, log a message, or take any other appropriate action.
+            // For simplicity, let's log a message to the console.
+            System.out.println("Role with ID " + newRole.getRoleID() + " already exists.");
+        }
     }
 
 }
