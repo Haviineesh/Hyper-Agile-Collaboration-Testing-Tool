e65f2f27ee048e79c2a60a0161cf8859fce52695
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-10 22:27:06 +0800

    william merge

--- demo/src/main/java/demo_ver/demo/service/ShowNotification.java
+++ demo/src/main/java/demo_ver/demo/service/ShowNotification.java
@@ -1,36 +1,36 @@
-package demo_ver.demo.service;
+// package demo_ver.demo.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class ShowNotification {
+// @SpringBootApplication
+// public class ShowNotification {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ShowNotification.class, args);
-    }
-}
+//     public static void main(String[] args) {
+//         SpringApplication.run(ShowNotification.class, args);
+//     }
+// }
 
-@RestController
-@RequestMapping("/api")
-class NotificationController {
+// @RestController
+// @RequestMapping("/api")
+// class NotificationController {
 
-    @GetMapping("/send-notification")
-    public String sendNotification() {
-        // Replace this with your actual notification logic
-        String notificationMessage = "Hello, this is a notification!";
-        sendNotificationToPostman(notificationMessage);
-        return "Notification sent successfully";
-    }
+//     @GetMapping("/send-notification")
+//     public String sendNotification() {
+//         // Replace this with your actual notification logic
+//         String notificationMessage = "Hello, this is a notification!";
+//         sendNotificationToPostman(notificationMessage);
+//         return "Notification sent successfully";
+//     }
 
-    private void sendNotificationToPostman(String message) {
-        // You can use any notification mechanism here, like sending an email, SMS, etc.
-        // For simplicity, we will print the message to the console.
-        System.out.println("Notification: " + message);
-    }
-}
+//     private void sendNotificationToPostman(String message) {
+//         // You can use any notification mechanism here, like sending an email, SMS, etc.
+//         // For simplicity, we will print the message to the console.
+//         System.out.println("Notification: " + message);
+//     }
+// }
