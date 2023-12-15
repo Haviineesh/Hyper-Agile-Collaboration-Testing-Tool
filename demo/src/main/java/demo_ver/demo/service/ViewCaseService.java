c294a0149191fb44d56b0eb5c15d76a5fc7f1c4e
amnizyy <46371731+amnizyy@users.noreply.github.com>
2023-12-15 15:52:38 +0800

    fix service shown

--- demo/src/main/java/demo_ver/demo/service/ViewCaseService.java
+++ demo/src/main/java/demo_ver/demo/service/ViewCaseService.java
@@ -4,42 +4,50 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import demo_ver.demo.model.TestCase;
+import demo_ver.demo.utils.RandomNumber;
+
+
 
 @Service
 public class ViewCaseService {
 
-     private static List<TestCase> testList = new ArrayList<TestCase>(Arrays.asList(
-            new TestCase(234,"Tester"),
-            new TestCase(334,"Proproduction"),
-            new TestCase(22, "Manager")));
+     private static List<TestCase> testList = new ArrayList<TestCase>(){
+     {
+            add(new TestCase(RandomNumber.getRandom(100,999),"001","Tester"));
+            add(new TestCase(RandomNumber.getRandom(100,999),"002","Proproduction"));
+            add(new TestCase(RandomNumber.getRandom(100,999), "003","Manager"));
+     }
+    };
+           
 
-    public static List<TestCase> getAllRoles() {
-        return new ArrayList<TestCase>(testList);
+    public static List<TestCase> findAllList() {
+       return testList;
     }
 
     public void addTestCaseForm(TestCase testCase) {
-        // Add the product to the static productList
+        
+        testCase.setIdtest_cases(RandomNumber.getRandom(100,999));
         testList.add(testCase);
     }
 
-    // public static void deleteById(int idtest_cases) {
-    //     testList.removeIf(testCase -> testCase.getId() == idtest_cases);
-    // }
+    public void deleteCase(long idtest_cases){
+        testList.removeIf(t ->t.getIdtest_cases()==idtest_cases);
+    }
 
-    // public boolean deleteCase(String name) {
-	//     Iterator<TestCase> iterator = testList.iterator();
-	//     while (iterator.hasNext()) {
-	//         TestCase testCase = iterator.next();
-	//         if (testCase.getName().equalsIgnoreCase(name)) {
-	//             iterator.remove();
-	//             return true; // Product found and deleted
-	//         }
-	//     }
-	//     return false; // Product not found
-	// }
+    public Optional <TestCase> findById(long idtest_cases){
+        return testList.stream().filter(t -> t.getIdtest_cases()==idtest_cases).findFirst();
+    }
+
+    public void updateCase(TestCase testCase){
+        deleteCase(testCase.getIdtest_cases());
+        testList.add(testCase);
+    }
+
+
+
 }
