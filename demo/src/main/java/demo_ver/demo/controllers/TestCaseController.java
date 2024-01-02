8fda18a9bce8ce1a3430519cde1496307ca1384f
amnizyy <46371731+amnizyy@users.noreply.github.com>
2024-01-02 23:23:37 +0800

    update to <List>Integer

--- demo/src/main/java/demo_ver/demo/controllers/TestCaseController.java
+++ demo/src/main/java/demo_ver/demo/controllers/TestCaseController.java
@@ -1,13 +1,18 @@
 package demo_ver.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import demo_ver.demo.model.TestCase;
+import demo_ver.demo.service.ManageUserService;
 import demo_ver.demo.service.ViewCaseService;
 
 
@@ -34,12 +39,13 @@ public class TestCaseController {
     @GetMapping("/add")
     public String showAddTestCaseForm(Model model) {
         model.addAttribute("testCase", new TestCase());
+        model.addAttribute("users", ManageUserService.getAllUsers());
         return "addTestCase"; // Thymeleaf template name for the addTestCase.html file
     }  
 
     @PostMapping("/save")
-	public String addTestCaseForm(TestCase testCase,Model model) {
-		viewCaseService.addTestCaseForm(testCase);// save product into database, using DbService
+	public String addTestCaseForm(TestCase testCase,@RequestParam("userID") List<Integer> userID,Model model) {
+		viewCaseService.addTestCaseForm(testCase,userID);// save product into database, using DbService
 		return "redirect:/view";
 	}
 
@@ -53,14 +59,16 @@ public class TestCaseController {
     @GetMapping("/editCase/{idtest_cases}")
 	public String editCase(@PathVariable("idtest_cases") Long idtest_cases,Model model) {
 		
-        model.addAttribute("testCase", viewCaseService.findById(idtest_cases));
+        TestCase userToAssign = viewCaseService.getTestCaseById(idtest_cases);
+        model.addAttribute("testCase", userToAssign);
+        model.addAttribute("users", ManageUserService.getAllUsers());
 		return "EditTestCase";
 	}
 
     @PostMapping("/update")
-	public String editTestCaseForm(TestCase testCase,Model model) {
+	public String editTestCaseForm(TestCase testCase,@RequestParam("userID") List<Integer>  userID,Model model) {
 		
-        viewCaseService.updateCase(testCase);
+        viewCaseService.updateCaseUser(testCase,userID);
 		return "redirect:/view";
 	}
 
