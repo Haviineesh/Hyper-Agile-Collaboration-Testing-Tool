37e8f0b367f48371662e7c33496172ee1d11c675
William Lau Lik Wei <147962021+Williamlau31@users.noreply.github.com>
2024-01-08 10:32:00 +0800

    Change the notification structure.

---
+++ demo/src/main/java/demo_ver/demo/mail/MailController.java
@@ -0,0 +1,23 @@
+package demo_ver.demo.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/mail")
+public class MailController {
+    @Autowired
+    private MailService mailService;
+
+    @PostMapping("/send/{mail}")
+    public String sendMail(@PathVariable String mail, @RequestBody MailStructure mailStructure) {
+        mailService.sendMail(mail, mailStructure);
+        return "Successfully sent the mail";
+    }
+}
+
---
+++ demo/src/main/java/demo_ver/demo/mail/MailService.java
@@ -0,0 +1,41 @@
+package demo_ver.demo.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    private String fromMail;
+
+    public void sendMail(String recipient, MailStructure mailStructure) {
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+            simpleMailMessage.setFrom(fromMail);
+            simpleMailMessage.setSubject(mailStructure.getSubject());
+            simpleMailMessage.setText(mailStructure.getMessage());
+            simpleMailMessage.setTo(recipient);
+
+            mailSender.send(simpleMailMessage);
+        } catch (MailException e) {
+            // Handle mail sending exceptions, e.g., log the error
+            // This is important for robustness and debugging
+            e.printStackTrace();
+        }
+    }
+
+    public void sendAssignedMail(String recipient, String subject, String message) {
+        MailStructure mailStructure = new MailStructure(recipient, subject, message);
+        sendMail(recipient, mailStructure);
+    }
+}
+
